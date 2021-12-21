057930b8ad158460aaa24d5adaba457a225ba3f0
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2021-12-22 01:05:28 +0530

    created navigation between pages

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from main import views
 
 urlpatterns=[
-    path('projects',views.projects),
-    path('experience',views.experience),
-    path('',views.index)
+    path('projects/',views.projects,name='projects'),
+    path('experience/',views.experience,name='experience'),
+    path('',views.index,name='index')
    
 ]
